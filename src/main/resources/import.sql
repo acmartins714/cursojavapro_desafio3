e452d78af97e5ea9b115f17a5207f8de4c830c3b
acmartins714 <acmartins714@gmail.com>
2026-02-18 02:34:12 -0300

    Commit após realização de testes e verificação de requisitos

---
+++ src/main/resources/import.sql
@@ -0,0 +1,12 @@
+-- Dados gerados pelo GEMINI em 16 de Fevereiro de 2026 às 23:56. O prompt utilizado foi:
+-- Gere 10 registros com dados aleatórios contendo: nome, cpf, Renda, Data de Nascimento, número de filhos
+INSERT INTO tb_client (name, cpf, income, birth_date, children) values ('Ricardo Alencar Souza', '12948530211', 4520.00, '1988-05-02', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) values ('Mariana Luzia Freitas','45021983405', 2150.50, '1995-11-28', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) values ('Thiago Cavalcante Silva','08237491182', 7800.00, '1982-02-03', 3);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) values ('Beatriz Farias Gomes','33109475623', 3200.00, '1999-07-15', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) values ('Felipe Augusto Neves','67412055890', 5430.20, '1975-07-22', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) values ('Juliana Meireles Castro','90211384744', 12600.00, '1990-01-30', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) values ('Roberto Ferreira Lima','21588409361', 1950.00, '2001-06-14', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) values ('Camila Viana Rocha','55301274418', 6100.00, '1985-10-08', 4);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) values ('Lucas Eduardo Silveira','41293055209', 3890.00, '1993-04-19', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) values ('Sandra Helena Martins','72844120357', 9250.00, '1979-12-25', 2);
